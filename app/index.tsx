d7423fe8ec348ddff2e75a1342ed62aba5dddbfc
Jeremy <jeremythegreat95@gmail.com>
2025-09-29 21:56:08 +0800

    tuned app name and added safe padding at bottom screen to avoid navigator overlay

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,26 @@
 import Calculator from '@/components/Calculator';
 import { Stack } from 'expo-router';
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { COLORS } from '@/constants/Theme';
 
 export default function Index() {
+  const insets = useSafeAreaInsets();
+
   return (
     <>
       <Stack.Screen options={{
         title: '',
         headerStyle: {
-          backgroundColor: COLORS.background,
+          backgroundColor: COLORS.background
         },
+        headerShadowVisible: false,
         headerTintColor: COLORS.defaultText
       }} />
-      <Calculator />
+
+      <SafeAreaView style={{ flex: 1, paddingBottom: insets.bottom, backgroundColor: COLORS.background }}>
+        <Calculator />
+      </SafeAreaView>
     </>
   );
 }
