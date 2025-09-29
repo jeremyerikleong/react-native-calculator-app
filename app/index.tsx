42e5425664f7be9ff910ee632a68649a9985156c
Jeremy <jeremythegreat95@gmail.com>
2025-09-29 19:53:49 +0800

    updated splash icon, adaptive icon and wording

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,18 @@
 import Calculator from '@/components/Calculator';
 import { Stack } from 'expo-router';
 
+import { COLORS } from '@/constants/Theme';
+
 export default function Index() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Calculator' }} />
+      <Stack.Screen options={{
+        title: '',
+        headerStyle: {
+          backgroundColor: COLORS.background,
+        },
+        headerTintColor: COLORS.defaultText
+      }} />
       <Calculator />
     </>
   );
